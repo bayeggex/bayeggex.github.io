03e646d10073a2674349a1e9986b208e60d83f52
Bay Eggex <bayeggex@gmail.com>
2024-05-25 23:23:17 +0300

    🤿

--- README.md
+++
@@ -1,2 +0,0 @@
-# bayeggex.github.io
-my website
---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const themeIcon = document.getElementById('theme-icon');
+    const body = document.body;
+
+    themeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        if (body.classList.contains('dark-mode')) {
+            themeIcon.classList.remove('fa-sun');
+            themeIcon.classList.add('fa-moon');
+        } else {
+            themeIcon.classList.remove('fa-moon');
+            themeIcon.classList.add('fa-sun');
+        }
+    });
+});
